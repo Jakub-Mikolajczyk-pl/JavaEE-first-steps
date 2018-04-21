fd9d1c1e3d61606421a8907b65ea2b747b5e4ee9
Xupi <jakubmikolajczyk@poczta.fm>
2018-04-21 21:37:19 +0200

    Added validation

--- src/test/java/com/pluralsight/bookstore/repository/com/pluralsight/bookstore/model/repository/BookRepositoryTest.java
+++ src/test/java/com/pluralsight/bookstore/repository/com/pluralsight/bookstore/model/repository/BookRepositoryTest.java
@@ -31,8 +31,19 @@ public class BookRepositoryTest {
     @Inject
     private BookRepository bookRepository;
 
+    @Test(expected = Exception.class)
+    public void findWithInvalidId(){
+        bookRepository.find(null);
+    }
+
+    @Test(expected = Exception.class)
+    public void createInvalidBook(){
+        Book book = new Book("isbn", null, 12F, 123, Language.ENGLISH, new Date(), "http://bllb", "description");
+        bookRepository.create(book);
+    }
+
     @Test
-    public void create() {
+    public void create() throws Exception{
         // Test counting books
         assertEquals(Long.valueOf(0), bookRepository.countAll());
         assertEquals(0, bookRepository.findAll().size());
